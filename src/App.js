b1ef57bee0fcd136ce7576195fa5cb64f063108e
candresra <carlos.ramirez-t@mail.escuelaing.edu.co>
2020-08-20 02:11:36 -0500

    :sparkles: - Store section client view

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {ClientPageStore} from './ClientPageStore'
+import {SearchProductStore} from './SearchProductStore';
+import {ProductStore} from './ProductStore';
+import {ProductListStore} from './ProductListStore';
 
 function App() {
+
+  const test = [{name: "Product1", price: 50000, description: "Comida"},
+                {name: "Product2", price: 50001, description: "Comida"},
+                {name: "Product3", price: 50002, description: "Comida"},
+                {name: "Product4", price: 50003, description: "Comida"},
+                {name: "Product5", price: 50004, description: "Comida"},
+                {name: "Product6", price: 50005, description: "Comida"},
+                {name: "Product7", price: 50006, description: "Comida"},
+                {name: "Product8", price: 50007, description: "Comida"},
+                {name: "Product9", price: 50008, description: "Comida"},];
   return (
     <div className="App">
-      <ClientPageStore />
+      <SearchProductStore />
+      <ProductListStore products={test}> </ProductListStore>
     </div>
   );
 }
---
+++ src/SearchProductStore.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+export class SearchProductStore extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: ''
+    };
+    this.handleSubmitSearch = this.handleSubmitSearch.bind(this);
+    this.handleChangeSearchBar = this.handleChangeSearchBar.bind(this);
+  }
+
+
+  handleSubmitSearch(event){
+    event.preventDefault();
+    console.log(this.state.search);
+  }
+
+
+  handleChangeSearchBar(event){
+    this.setState({ search: event.target.value});
+  }
+
+
+  render(){
+    return (
+        <div>
+          <form onSubmit={this.handleSubmitSearch}>
+            <div className="input-group mb-3">
+              <input type="text" onChange={this.handleChangeSearchBar} className="form-control" placeholder="Product" aria-label="Product" aria-describedby="button-addon2" />
+              <div className="input-group-append">
+                <button className="btn btn-outline-secondary" type="submit" id="search">Search</button>
+              </div>
+            </div>
+          </form>
+        </div>
+    );
+  }
+
+}
